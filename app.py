d576d66b1588c1fe8c3fd6a790d286c90dfdd36f
Diavats <xyzatlcs@gmail.com>
2025-05-26 06:14:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,8 @@ def chat():
             break
     return jsonify({"reply": reply})
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
